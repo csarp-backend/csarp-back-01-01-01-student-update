a2bf9fd6292dd5bb73cce55459657906b1de623f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-03 11:23:05 +0100

    ControllerResponse

--- Kreata.Backend/Datas/Responses/ControllerResponse.cs
+++ Kreata.Backend/Datas/Responses/ControllerResponse.cs
@@ -1,6 +1,9 @@
 ﻿namespace Kreata.Backend.Datas.Responses
 {
-    public class ControllerResponse
+    public class ControllerResponse : ErrorStore
     {
+        public bool IsSuccess => !HasError;
+
+        public ControllerResponse() : base() { }
     }
 }
